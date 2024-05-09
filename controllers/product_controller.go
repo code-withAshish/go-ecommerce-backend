d9d3fa69b5007d864a68065c351ca3e45a56a6f0
Ashish Bhushan Kumar <73625149+code-withAshish@users.noreply.github.com>
2024-05-10 00:43:21 +0530

    basic setup

--- controllers/product_controller.go
+++ controllers/product_controller.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"backend/utils"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
+
+var db = utils.GetDatabaseConnection()
 
 func GetProductById(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, echo.Map{"route": "product route controller"})
